a2e41d9417c1cccb87143c1deb5d3bc9d9397232
Matthias Bauer <moeffju@gmail.com>
2011-05-13 10:12:51 +0200

    Add more views

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Plusplus::Application.routes.draw do
   root :to => 'home#index'
   match "/rsvp" => 'home#rsvp'
-
+  get "/impressum" => 'home#impressum'
+  get "/sponsoring" => 'home#sponsoring'
+  get "/credits" => 'home#credits'
   
   match "/auth/:provider/callback" => "sessions#create"
   match "/auth/failure" => "home#index"
   get '/signin' => "sessions#signin"
   get "/signout" => "sessions#destroy", :as => :signout
-
 end
