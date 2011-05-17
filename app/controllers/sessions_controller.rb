f0ff2056540a837b39973cb973f0e0d4b50b3812
Matthias Bauer <moeffju@gmail.com>
2011-05-17 19:20:39 +0200

    Automatically put users on the Yes list

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,7 +1,11 @@
 class SessionsController < ApplicationController
   def create
     auth = request.env["omniauth.auth"]
-    user = User.find_by_provider_and_uid(auth["provider"], auth["uid"]) || User.create_with_omniauth(auth)
+    unless user = User.find_by_provider_and_uid(auth["provider"], auth["uid"])
+      user = User.create_with_omniauth(auth)
+      user.status = 1
+      user.save
+    end
     session[:user_id] = user.id
     redirect_to root_url, :notice => "Angemeldet!"
   end
