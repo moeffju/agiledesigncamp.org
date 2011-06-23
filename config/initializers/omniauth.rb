023ca1c9f0288b1df7d90ffd8a0a21030e262c8e
Jan Krutisch <jan@krutisch.de>
2011-06-23 23:02:18 +0200

    removing credentials, making the ENV configurable

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,4 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :twitter, 'g7Gl5khqmMngmIG9wfwmPg', 'S53fYPaTlzc5QQz44dXiPbmOWUnyuEVjgdbr49oj0'
-  provider :facebook, '209358412420026', '002cfef0d935fb0b9f7ff29de3db7a62', :scope => ''
+  provider :twitter, ENV['TWITTER_API_KEY'], ENV['TWITTER_API_SECRET']
+  provider :facebook, ENV['FACEBOOK_API_KEY'], ENV['FACEBOOK_API_SECRET'], :scope => ''
 end
