282b4345d38cca24dd6bd78f35029269f14a49d9
Jan Krutisch <jan@krutisch.de>
2011-06-23 23:09:36 +0200

    redirecting domains

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,8 @@ class ApplicationController < ActionController::Base
 
   helper_method :current_user, :is_admin?
   
+  before_filter :redirect_domains
+  
   def ensure_admin
     raise ActionController::RoutingError.new('Not Found') unless is_admin?
   end
@@ -16,4 +18,13 @@ class ApplicationController < ActionController::Base
   def is_admin?
     current_user && %w(5618832 14583478).include?(current_user.uid)
   end
+  
+  def redirect_domains
+    if Rails.env.production?
+      unless request.host == 'agiledesigncamp.org'
+        redirect_to "http://agiledesigncamp.org" and return false
+      end
+    end
+  end
+  
 end
