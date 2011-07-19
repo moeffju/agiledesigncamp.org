7263dbcfa5f0f09acac7e14068542d293191a180
Matthias Bauer <moeffju@gmail.com>
2011-07-19 18:24:42 +0200

    change sponsors layout

--- app/models/sponsor.rb
+++ app/models/sponsor.rb
@@ -1,3 +1,12 @@
 class Sponsor < ActiveRecord::Base
   default_scope :order => 'amount DESC, created_at ASC'
+  
+  def level
+    case self.amount
+    when 0..499; :supporter;
+    when 500..799; :silver;
+    when 800..1999; :gold;
+    when 2000..1000000000; :main;
+    end
+  end
 end
